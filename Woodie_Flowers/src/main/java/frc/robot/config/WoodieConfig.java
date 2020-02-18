0b6a6e07d781a6927c36efe44760861ab8f00b43
RSchade <Hackbots@10.240.193.136>
2020-02-17 20:28:26 -0500

    Robot Config Framework

---
+++ Woodie_Flowers/src/main/java/frc/robot/config/WoodieConfig.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.config;
+
+import java.util.HashMap;
+
+/**
+ * Add your docs here.
+ */
+public class WoodieConfig {
+    public static HashMap<String, Double> getConfig() {
+        HashMap<String, Double> config = new HashMap<>();
+        config.put("", 0.0);
+
+        return config; 
+    }
+}
