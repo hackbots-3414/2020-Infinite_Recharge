0d88cff0805a2de1369c6d268c33e3ca5493f048
Tyler <tylernewton08@gmail.com>
2020-02-25 18:07:10 -0500

    Pushing new belt numbers

--- Woodie_Flowers/src/main/java/frc/robot/commands/LEDDefaultCommand.java
+++ Woodie_Flowers/src/main/java/frc/robot/commands/LEDDefaultCommand.java
@@ -33,7 +33,7 @@ public class LEDDefaultCommand extends CommandBase {
   @Override
   public void execute() {
     // System.out.println("Led.DefaultCommand.Execute()");
-    ledSubsystem.switchOnLEDs(ColorSystem.COLOR_LIGHT_BLUE, ColorSystem.COLOR_PATTERN_PULSE);
+   // ledSubsystem.switchOnLEDs(ColorSystem.COLOR_LIGHT_BLUE, ColorSystem.COLOR_PATTERN_PULSE);
   }
 
   // Called once the command ends or is interrupted.
