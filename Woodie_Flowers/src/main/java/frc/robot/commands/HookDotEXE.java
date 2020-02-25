5f139949f75aeeb7f3d5b6c442c8e284477b4f5e
myusername <myemail>
2020-02-25 18:04:44 -0500

    Climber with commands

--- Woodie_Flowers/src/main/java/frc/robot/commands/HookDotEXE.java
+++ Woodie_Flowers/src/main/java/frc/robot/commands/HookDotEXE.java
@@ -8,11 +8,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.HookSubsystem;
+import frc.robot.subsystems.PulleySubsystem;
+import frc.robot.teleop.OI;
 
 public class HookDotEXE extends CommandBase {
   /**
    * Creates a new HookDotEXE.
    */
+  HookSubsystem hook = new HookSubsystem();
   public HookDotEXE() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -20,11 +24,13 @@ public class HookDotEXE extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    hook.setEncoder(0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    hook.setHook(OI.getXboxController().getRawAxis(1));
   }
 
   // Called once the command ends or is interrupted.
@@ -35,6 +41,10 @@ public class HookDotEXE extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(hook.getEncoder()> 1000000000){
+      hook.setEncoder(0);
+      return true;
+    }
     return false;
   }
 }
--- Woodie_Flowers/src/main/java/frc/robot/commands/PulleyDotEXE.java
+++ Woodie_Flowers/src/main/java/frc/robot/commands/PulleyDotEXE.java
@@ -8,11 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PulleySubsystem;
+import frc.robot.teleop.OI;
 
 public class PulleyDotEXE extends CommandBase {
   /**
    * Creates a new PulleyDotEXE.
    */
+  PulleySubsystem pully = new PulleySubsystem();
   public PulleyDotEXE() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -20,12 +23,13 @@ public class PulleyDotEXE extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    pully.setEncoder(0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    pully.setPullEy(OI.getXboxController().getRawAxis(3));
   }
 
   // Called once the command ends or is interrupted.
@@ -36,6 +40,10 @@ public class PulleyDotEXE extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(pully.getEncoder()> 1000000000){
+      pully.setPullEy(0);
+      return true;
+    }
     return false;
   }
 }
